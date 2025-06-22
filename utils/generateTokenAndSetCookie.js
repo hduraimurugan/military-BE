255df94f18bf7d25f47c6b3236a5034a7e122a48
Duraimurugan-H <hduraimurugan@gmail.com>
2025-06-22 21:39:50 +0530

    updated refresh token timings

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -21,7 +21,7 @@ export const generateTokenAndSetCookie = (res, payload) => {
         sameSite: isProduction ? "none" : "lax",
         // sameSite: 'None',
         secure: isProduction,
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     })
 
     return { accessToken, refreshToken };
